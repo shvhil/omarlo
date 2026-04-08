ea2a807a04903aad7f1dd88d23376117f7f7747c
Omarlo <omarlo.mahmoud2017@gmail.com>
2026-04-08 16:28:53 +0300

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,15 @@ import './App.css';
 
 const SiteLogo = ({ loading }) => {
   return (
-    <div 
-      className={`site-logo ${!loading ? 'visible' : ''}`} 
+    <div
+      className={`site-logo ${!loading ? 'visible' : ''}`}
       onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
     >
       <svg viewBox="0 0 1920 1080">
         <g>
-          <polygon fill="currentColor" points="1144.1,339.6 66.2,543.8 55.4,637.8 1133.3,433.7"/>
-          <path fill="currentColor" d="M1497.6,437.5l9.8-59.8l-158.2-39.1c-43.1-10.7-86.1,17.7-93.3,61.5l-11.1,67.8L269.6,652.6l-13.5,94.6 l1230.6-233.1L1497.6,437.5z"/>
-          <polygon fill="currentColor" points="1815.1,415.1 1830.6,320.1 1678.2,349 1589.3,365.8 1558.3,555.8 454.5,764.9 443.2,859 1542.8,650.7 1631.6,633.9 1788.4,604.2 1799.6,510.1 1647.1,539 1662.6,444"/>
+          <polygon fill="currentColor" points="1144.1,339.6 66.2,543.8 55.4,637.8 1133.3,433.7" />
+          <path fill="currentColor" d="M1497.6,437.5l9.8-59.8l-158.2-39.1c-43.1-10.7-86.1,17.7-93.3,61.5l-11.1,67.8L269.6,652.6l-13.5,94.6 l1230.6-233.1L1497.6,437.5z" />
+          <polygon fill="currentColor" points="1815.1,415.1 1830.6,320.1 1678.2,349 1589.3,365.8 1558.3,555.8 454.5,764.9 443.2,859 1542.8,650.7 1631.6,633.9 1788.4,604.2 1799.6,510.1 1647.1,539 1662.6,444" />
         </g>
       </svg>
     </div>
@@ -81,7 +81,7 @@ function AppInner() {
     const onMove = (e) => {
       gsap.to(cursor, { x: e.clientX, y: e.clientY, duration: 0.35, ease: 'power2.out' });
       gsap.to(dot, { x: e.clientX, y: e.clientY, duration: 0.1 });
-      if (glow) gsap.to(glow, { x: e.clientX - 200, y: e.clientY - 200, duration: 1.5, ease: 'power2.out' });
+      if (glow) gsap.to(glow, { x: e.clientX, y: e.clientY, duration: 1.5, ease: 'power2.out' });
     };
 
     window.addEventListener('mousemove', onMove);
